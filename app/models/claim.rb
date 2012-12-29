c5374604e9531733ec29fd4da8a2479eb9c2d8f4
Krister Bäckman <ixevix@gmail.com>
2012-12-29 14:31:15 +0200

    Add dev fix to claims

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,10 +1,15 @@
 class Claim < ActiveRecord::Base
-  fields do
-    timestamps
+  if Rails.env == "development"
+    fields do
+      timestamps
+    end
   end
+
   belongs_to :user
   belongs_to :game_instance
   belongs_to :line
+
   validates_presence_of :user, :game_instance, :line
+
   index [:user_id, :game_instance_id], :unique => true
 end
