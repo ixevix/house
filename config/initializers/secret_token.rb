f08fe54eb50874e6da5ebc06973a42d05403fa3e
Krister Bäckman <ixevix@ubuntu.(none)>
2012-12-29 04:25:07 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+House::Application.config.secret_token = 'e2165d4583203baa306715e3875973fda37f28d8ec1009a7271b6e0fc56135c75df379a5eeb0a6a31a295e6d58515548ed5e632d5a445104d7d7ef4e5d7517f6'
