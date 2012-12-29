f412c61576a9164972e0b4ecce2faec4726ee3a2
Daniel Dunderfelt <dunderfeltdaniel@gmail.com>
2012-12-29 17:23:56 +0200

    first iteration highlighting

--- public/script.js
+++ public/script.js
@@ -1,13 +1,60 @@
 var $square;
+var $body;
+
+var hilightLine = function(actLine, squareId) {
+
+    $("#"+squareId).css("border", "none").css(actLine, "3px solid black");
+    console.log(squareId);
+};
 
 $(function() {
     $square = $(".square");
+    $body = $("body");
 
-    $square.mousedown(function() {
-        console.log("embiggen");
-        $(this).transition({
-            scale: 1.5
-        }, 200);
+    $body.mouseup(function() {
+        $square.stop(true, true).transition({
+            scale: 1
+        }, 100);
+        $body.off("mousemove.square");
     });
+
+    $square.mousedown(function(e) {
+        $(this).stop(true, true).transition({
+            scale: 1.3
+        }, 100);
+
+        var squareId = $(this).prop("id");
+
+        var mX = e.pageX;
+        var mY = e.pageY;
+
+        $body.on("mousemove.square", function(e) {
+            var dmX = e.pageX - mX;
+            var dmY = mY - e.pageY;
+
+            var aX = Math.abs(dmX);
+            var aY = Math.abs(dmY);
+
+            var actLine;
+
+            if ( aX > aY ) {
+                if ( dmX >= 0 ) {
+                    actLine = "border-right";
+                } else {
+                    actLine = "border-left";
+                }
+            } else if ( dmY >= 0 ) {
+                actLine = "border-top";
+            } else {
+                actLine = "border-bottom";
+            }
+
+            hilightLine(actLine, squareId);
+
+            $("#console").text(actLine);
+        });
+
+    });
+
 
 });
